cb42485c1080448e39d157240628cfb2dfbf08f2
Marc Neuhaus <marc@neuhaus.nrw>
2025-02-07 18:06:40 +0100

    added dummy tracks node

--- gamepad/gamepad/main.py
+++ gamepad/gamepad/main.py
@@ -8,17 +8,7 @@ def main():
     gp = Gamepad()
 
     for event in node:
-        if event["type"] == "INPUT":
-            if event["id"] == "TICK":
-                print(
-                    f"""Node received:
-                id: {event["id"]},
-                value: {event["value"]},
-                metadata: {event["metadata"]}"""
-                )
         event = gp.getNextEvent()
-
-        print(f"Publishing gamepad event: {event}")
         node.send_output(
             output_id="gamepad_input", data=pa.array([str(event)]), metadata={}
         )
